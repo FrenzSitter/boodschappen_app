a1fe27f3851e6a4d06d1c094fbbe8142db70c7bc
FrenzSitter <FrenzSitter@users.noreply.github.com>
2025-08-14 19:49:22 +0000

    deploy: 0c88813d22d2137d7354e44881254d17be08018b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "651e3b80d7cb65d985e09837cf5ee543",
+const RESOURCES = {"flutter_bootstrap.js": "a3105a4bc4e1f8ab4739cab92009e45d",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
